853e5955c080afade4b501873b6000bcdcc88b56
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-11 16:21:33 +0200

    aggiunto cambio colore font

--- script.js
+++ script.js
@@ -6,7 +6,19 @@ createApp ({
     data() {
         return {
             message:'Hello World!',
-            myImg: 'https://picsum.photos/500/500'
+            myImg: 'https://picsum.photos/500/500',
+            fontColor:'text-red'
+        }
+    },
+    methods: {
+        changeColor: function() {
+
+            console.log(this.fontColor);
+            if (this.fontColor == 'text-red') {
+                this.fontColor = 'text-green';
+            } else {
+                this.fontColor = 'text-red';
+            }
         }
     }
 }).mount('#app')
