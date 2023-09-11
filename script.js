364b430bb9b15a3fc2f4007f7ec79d3d474e7ef0
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-11 16:05:15 +0200

    aggiunta immgaine
    agguinta alla pagina un'immagine salvata in data()

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const { createApp } = Vue
 createApp ({
     data() {
         return {
-            message:'Hello World!'
+            message:'Hello World!',
+            myImg: 'https://picsum.photos/500/500'
         }
     }
 }).mount('#app')
